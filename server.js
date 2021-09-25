fe153a72d9b4f2a75abee95860f2e7b93e7a8fb8
kritish bhattarai <rishi58rishi@gmail.com>
2021-09-26 00:14:34 +0545

    added home page

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const db = require('./db');
+
+app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get('/userDetails', (req, res) => {
    return res.status(200).json(db.userDetails);
@@ -26,6 +29,10 @@ app.put('/items/:itemId', (req, res) => {});
 app.delete('/items/:itemId', (req, res) => {
    db.items = db.items.filter((item) => item.id.toString() == req.params.itemId);
    return res.status(200).json(db.items);
+});
+
+app.get('*', (req, res) => {
+   return res.sendFile(path.join(__dirname, 'pages', 'index.html'));
 });
 
 app.listen(process.env.PORT || 5000, () => {
